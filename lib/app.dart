e980e3583339d9f17617274fc9ec3ec6be2ac4d2
DmFedoruk <Dmi.Fedoruk@yandex.com>
2023-10-31 14:09:12 +0300

    project creation and preparation for development

---
+++ lib/app.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+import 'screens/home/home_screen.dart';
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(home: HomeScreen());
+  }
+}
---
+++ lib/styles/text_string.dart
@@ -0,0 +1,41 @@
+class TextString {
+  static const String greetingsString = 'Good morning';
+  static const List<String> shortNamesDaysOfWeek = [
+    'Mon',
+    'Tue',
+    'Wed',
+    'Thu',
+    'Fri',
+    'Sat',
+    'Sun'
+  ];
+
+  static const String monday = 'Monday';
+  static const String thuesday = 'Thuesday';
+  static const String wednesday = 'Wednesday';
+  static const String thursday = 'Thursday';
+  static const String friday = 'Friday';
+  static const String saturday = 'Saturday';
+  static const String sunday = 'Sunday';
+
+  static const String today = 'Today';
+  static const String yesterday = 'Yesterday';
+  static const String tomorrow = 'Tomorrow';
+
+  static const String list = 'List';
+  static const String noList = 'No list';
+  static const String newList = 'New list';
+  static const String writeTask = 'Write your task';
+  static const String task = 'task';
+  static const String tasks = 'tasks';
+
+  static const String search = 'Search task of list';
+
+  static const String statistics = 'Statistics';
+  static const String weekly = 'Weekly';
+  static const String moonthly = 'Monthly';
+  static const String yearly = 'Yearly';
+  static const String all = 'All';
+  static const String taskCompleted = 'Task completed';
+  static const String completionRate = 'Completion rate';
+}
